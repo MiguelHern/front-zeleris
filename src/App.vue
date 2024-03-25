930b7cdd76ce5042812dbb90769eea694c796181
MiguelHern <al066382@uacam.mx>
2024-03-24 22:35:04 -0600

    Correcciones

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+
+import Pru from "@/components/pru.vue";
+</script>
+
+<template>
+  <pru></pru>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/pru.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<h1 class="lza"> Miguel</h1>
+  <h3>asasaaaa</h3>
+</template>
+
+<style scoped>
+h3{
+  color: var(--principal-color);
+}
+</style>
