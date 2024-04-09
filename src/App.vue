d56b26a974151a4c57d02663d55fc49432d73ad7
DevaDTC <DevanyDayana@Gmail.com>
2024-04-09 15:32:20 -0600

    I made the view Generate Economic Permit 2 Intento

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 
 import Header__docente from "@/components/header__docente.vue";
+import geneEcoPerComponent from "@/components/geneEcoPer_docente.vue";
 </script>
 
 <template>
   <router-view></router-view>
+  <geneEcoPer_docente></geneEcoPer_docente>
 </template>
 
 <style scoped>
