eae43521f2e3c76d6e5863cbe054a58277cd1187
MiguelHern <al066382@uacam.mx>
2024-04-30 14:45:54 -0600

    Agregados archivos no rastreados antes de fusionar cambios remotos

---
+++ src/views/Layout/header.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+<header>
+  <div class="container__img">
+  <img src="/src/assets/img/Zeleris.jpg">
+  </div>
+  <h1 class="header__company">Zeleris</h1>
+  <h1 class="header__tipo">Docentes</h1>
+</header>
+</template>
+
+<style scoped>
+header{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+.container__img{
+  width: 50px;
+  height: 50px;
+}
+.header__company{
+  font-size: 1.6rem;
+  margin-right: 1.2rem;
+}
+.header__tipo{
+  font-size: 1.4rem;
+}
+
+</style>
